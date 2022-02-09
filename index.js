03a3b292cd3f4ee654d3bb4137595fd927401bec
Jenn Robbins <Jennrobbins3@gmail.com>
2022-02-08 21:11:38 -0500

    Done with assignment

---
+++ index.js
@@ -0,0 +1,8 @@
+const divide = () => 2000 / 100;
+//divide(2000,100);
+
+const square = (parameter1) => parameter1 * parameter1;
+//square(2,2)
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+//add(4,5);
